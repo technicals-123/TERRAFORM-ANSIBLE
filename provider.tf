3d2a5f6a613378a83623cb9d8eb21d2dd30075d0
pranavkumarpk01 <124168954+pranavkumarpk01@users.noreply.github.com>
2024-03-20 12:47:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    # Specify your provider here
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+    # Add other providers if needed
+  }
+  provider_installation {
+    # Skip provider registration
+    skip_provider_registration = true
+  }
+}
 provider "azurerm" {
   features {}
 }
